55249ea4f0c35c28cfc548f9d7fd48388843bcfc
Chang Qiao <61445135+qc17-THU@users.noreply.github.com>
2022-06-12 22:56:30 +0800

    Add files via upload
    Update the MATLAB script for data augmentation. Users can generate augmented dataset of different biological specimens via simply modifying the variable "Specimen" without other changes.

--- data_agmt_matlab/DataAugumentation_ForTrain.m
+++ data_agmt_matlab/DataAugumentation_ForTrain.m
@@ -11,10 +11,12 @@ addpath(genpath('./XxUtils'));
 % SavePath/
 %
 % Before running, you should set following parameters first
+% Specimen: Select the biological specimen in CCPs, ER, Microtubules,
+% F-actin, F-actin_Nonlinear
 % SavePath: directory to save augumented data
 % DataPath: directory of BioSR dataset, eg: '../dataset/BioSR/F-actin';
-% DataFilter: raw SIM data in BioSR
-% GtFilter: GT-SIM data in BioSR
+% DataFilter: formatting of raw SIM data in BioSR
+% GtFilter: formatting of GT-SIM data in BioSR
 % RotFlag: 0 for no rotatioin, 1 for random rotation, 2 for flipping and random rotaion
 % SaveRawFlag: 0 for only saving WF patches, 1 for saving raw SIM image patches
 % SegX: target patch size x
@@ -29,28 +31,30 @@ addpath(genpath('./XxUtils'));
 %
 % Author: Chang Qiao
 % Email: qc17@mails.tsinghua.edu.cn
-% Version: 2020/5/15
+% Version: 2022/6/12
 % ------------------------------------------------------------------------
 
 %% set path and segmentation parameters
-SavePath = '../dataset/train/F-actin'; 
-DataPath = '../dataset/BioSR/F-actin';
-DataFilter = 'RawSIMData_level_*.mrc';
-GtFilter = 'SIM_gt*.mrc';
+Specimen = 'F-actin'; % CCPs | ER | Microtubules | F-actin | F-actin_Nonlinear
 
-RotFlag = 2;
-SaveRawFlag = 1;
+SavePath = ['../dataset/train/' Specimen]; 
+DataPath = ['../dataset/BioSR/' Specimen];
+DataFilter = 'RawSIMData_level_*.mrc'; % formatting of raw SIM data in BioSR
+GtFilter = 'SIM_gt*.mrc'; % formatting of GT-SIM data in BioSR
 
-SegX = 128;
-SegY = 128;
-ValidateNum = 2000;
-TotalSegNum = 20000;
+RotFlag = 2;     % 0 for no rotatioin, 1 for random rotation, 2 for flipping and random rotaion
+SaveRawFlag = 1; % 0 for only saving WF patches, 1 for saving raw SIM image patches
 
-bg_cut_prct = 0;
-train_cell_list = 6:35;
-train_snr_list = 1:12; 
-validate_cell_list = 1:5;
-validate_snr_list = 1:12;
+SegX = 128; % target patch size x
+SegY = 128; % target patch size y
+ValidateNum = 2000;  % number of patch pairs for validation
+TotalSegNum = 20000; % number of patch pairs for training
+
+bg_cut_prct = 0; % percentile value of gt background, used for some specific data, eg. CCPs
+train_cell_list = 6:35; % cells used for generating training image patch pairs
+train_snr_list = 1:12;  % snr levels used for generating training image patch pairs 
+validate_cell_list = 1:5; % cells used for generating validation image patch pairs
+validate_snr_list = 1:12; % snr levels used for generating validation image patch pairs
 
 %% initialization
 % if processing Non-Linear SIM data
@@ -84,41 +88,61 @@ mkdir(save_validate_gt_path), mkdir(save_validate_WF_path);
 %% Generate validate patches
 CellList = XxSort(XxDir(DataPath, 'Cell*'));
 ImgCount = 1;
+nCell = length(CellList);
+if sum(validate_cell_list > nCell) > 0
+    validate_cell_list(validate_cell_list > nCell) = [];
+    validate_cell_num = length(validate_cell_list);
+    warning('The cell list used for validation has been changed!');
+end
+
 for i = validate_cell_list
-   
+    
     % read data
-    files_input = XxSort(XxDir(CellList{i}, DataFilter));
-    files_gt = XxDir(CellList{i}, GtFilter);
-    [header_gt, data_gt] = XxReadMRC(files_gt{1});
-    data_gt = reshape(data_gt, header_gt(1), header_gt(2), header_gt(3));
-    data_gt = XxNorm(data_gt,0,100);
-    data_gt = imadjust(data_gt,[bg_cut_prct,1],[]);
+    if strcmp(Specimen, 'ER')
+        files_input = XxSort(XxDir([CellList{i} filesep 'RawSIMData'], DataFilter));
+        files_gt = XxSort(XxDir([CellList{i} filesep 'GTSIM'], 'GTSIM*'));
+    else
+        files_input = XxSort(XxDir(CellList{i}, DataFilter));
+        files_gt = XxDir(CellList{i}, GtFilter);
+        [header_gt, data_gt] = XxReadMRC(files_gt{1});
+        data_gt = reshape(data_gt, header_gt(1), header_gt(2), header_gt(3));
+        data_gt = XxNorm(data_gt,0,100);
+        data_gt = imadjust(data_gt,[bg_cut_prct,1],[]);
+    end
     
     snr_num = length(validate_snr_list);
     if validate_snr_list(end) > length(files_input)
         validate_snr_list = validate_snr_list(1):length(files_input);
+        warning(['The SNR range of validation data has been changed into '...
+            num2str(validate_snr_list(1)) ':' num2str(length(files_input))]);
         snr_num = length(validate_snr_list);
     end
     
     for j = validate_snr_list
-        fprintf('Generating validate data %d/%d, SNR %d/%d\n',...
+        fprintf('Generating validation data %d/%d, SNR %d/%d\n',...
             i-validate_cell_list(1)+1,validate_cell_num,j,snr_num);
         [header, data] = XxReadMRC(files_input{j});
         data = reshape(data, header(1), header(2), header(3));
         data = XxNorm(data,0,100);
+        if strcmp(Specimen, 'ER')
+            [header_gt, data_gt] = XxReadMRC(files_gt{j});
+            data_gt = reshape(data_gt, header_gt(1), header_gt(2), header_gt(3));
+            data_gt = XxNorm(data_gt,0,100);
+            data_gt = imadjust(data_gt,[bg_cut_prct,1],[]);
+        end
         
         if RotFlag < 2
             [data_seg, data_gt_seg] = XxDataSeg_ForTrain(data, data_gt, ...
-                round(ValidateNum/validate_cell_num/snr_num) , SegX, SegY, RotFlag);
+                ceil(ValidateNum/validate_cell_num/snr_num) , SegX, SegY, RotFlag);
             num_image = size(data_gt_seg, 3);
         else
             [data_seg, data_gt_seg] = XxDataSeg_ForTrain(data, data_gt, ...
-                round(ValidateNum/validate_cell_num/snr_num/4) , SegX, SegY, RotFlag);
+                ceil(ValidateNum/validate_cell_num/snr_num/4) , SegX, SegY, RotFlag);
             % flip axis y
             curdata = flipud(data);
             curdata_gt = flipud(data_gt);
             [data_seg_ap, data_gt_seg_ap] = XxDataSeg_ForTrain(curdata, curdata_gt, ...
-                round(ValidateNum/validate_cell_num/snr_num/4) , SegX, SegY, RotFlag);
+                ceil(ValidateNum/validate_cell_num/snr_num/4) , SegX, SegY, RotFlag);
             num_image = size(data_gt_seg_ap, 3);
             data_seg(:, :, end+1:end+num_image*n_perSIM) = data_seg_ap;
             data_gt_seg(:, :, end+1:end+num_image) = data_gt_seg_ap;
@@ -126,7 +150,7 @@ for i = validate_cell_list
             curdata = fliplr(data);
             curdata_gt = fliplr(data_gt);
             [data_seg_ap, data_gt_seg_ap] = XxDataSeg_ForTrain(curdata, curdata_gt, ...
-                round(ValidateNum/validate_cell_num/snr_num/4) , SegX, SegY, RotFlag);
+                ceil(ValidateNum/validate_cell_num/snr_num/4) , SegX, SegY, RotFlag);
             num_image = size(data_gt_seg_ap, 3);
             data_seg(:, :, end+1:end+num_image*n_perSIM) = data_seg_ap;
             data_gt_seg(:, :, end+1:end+num_image) = data_gt_seg_ap;
@@ -134,7 +158,7 @@ for i = validate_cell_list
             curdata = flipud(curdata);
             curdata_gt = flipud(curdata_gt);
             [data_seg_ap, data_gt_seg_ap] = XxDataSeg_ForTrain(curdata, curdata_gt, ...
-                round(ValidateNum/validate_cell_num/snr_num/4) , SegX, SegY, RotFlag);
+                ceil(ValidateNum/validate_cell_num/snr_num/4) , SegX, SegY, RotFlag);
             num_image = size(data_gt_seg_ap, 3);
             data_seg(:, :, end+1:end+num_image*n_perSIM) = data_seg_ap;
             data_gt_seg(:, :, end+1:end+num_image) = data_gt_seg_ap;
@@ -165,21 +189,33 @@ for i = validate_cell_list
 end
 
 %% Generate training patches
+if sum(train_cell_list > nCell) > 0
+    train_cell_list(train_cell_list > nCell) = [];
+    train_cell_num = length(train_cell_list);
+    warning('The cell list used for training has been changed!');
+end
 ImgCount = 1;
 for i = train_cell_list
     
     % read data
-    files_input = XxSort(XxDir(CellList{i}, DataFilter));
-    files_gt = XxDir(CellList{i}, GtFilter);
-    [header_gt, data_gt] = XxReadMRC(files_gt{1});
-    data_gt = reshape(data_gt, header_gt(1), header_gt(2), header_gt(3));
-    data_gt = XxNorm(data_gt,0,100);
-    data_gt = imadjust(data_gt,[bg_cut_prct,1],[]);
+    if strcmp(Specimen, 'ER')
+        files_input = XxSort(XxDir([CellList{i} filesep 'RawSIMData'], DataFilter));
+        files_gt = XxSort(XxDir([CellList{i} filesep 'GTSIM'], 'GTSIM*'));
+    else
+        files_input = XxSort(XxDir(CellList{i}, DataFilter));
+        files_gt = XxDir(CellList{i}, GtFilter);
+        [header_gt, data_gt] = XxReadMRC(files_gt{1});
+        data_gt = reshape(data_gt, header_gt(1), header_gt(2), header_gt(3));
+        data_gt = XxNorm(data_gt,0,100);
+        data_gt = imadjust(data_gt,[bg_cut_prct,1],[]);
+    end
     
-    snr_num = length(validate_snr_list);
-    if validate_snr_list(end) > length(files_input)
-        validate_snr_list = validate_snr_list(1):length(files_input);
-        snr_num = length(validate_snr_list);
+    snr_num = length(train_snr_list);
+    if train_snr_list(end) > length(files_input)
+        train_snr_list = train_snr_list(1):length(files_input);
+        warning(['The SNR range of training data has been changed into '...
+            num2str(train_snr_list(1)) ':' num2str(length(files_input))]);
+        snr_num = length(train_snr_list);
     end
     
     for j = train_snr_list
@@ -189,19 +225,25 @@ for i = train_cell_list
         [header, data] = XxReadMRC(files_input{j});
         data = reshape(data, header(1), header(2), header(3));
         data = XxNorm(data,0,100);
+        if strcmp(Specimen, 'ER')
+            [header_gt, data_gt] = XxReadMRC(files_gt{j});
+            data_gt = reshape(data_gt, header_gt(1), header_gt(2), header_gt(3));
+            data_gt = XxNorm(data_gt,0,100);
+            data_gt = imadjust(data_gt,[bg_cut_prct,1],[]);
+        end
         
         if RotFlag < 2
             [data_seg, data_gt_seg] = XxDataSeg_ForTrain(data, data_gt, ...
-                round(TotalSegNum/train_cell_num/snr_num) , SegX, SegY, RotFlag);
+                ceil(TotalSegNum/train_cell_num/snr_num) , SegX, SegY, RotFlag);
             num_image = size(data_gt_seg, 3);
         else
             [data_seg, data_gt_seg] = XxDataSeg_ForTrain(data, data_gt, ...
-                round(TotalSegNum/train_cell_num/snr_num/4) , SegX, SegY, RotFlag);
+                ceil(TotalSegNum/train_cell_num/snr_num/4) , SegX, SegY, RotFlag);
             % flip axis y
             curdata = flipud(data);
             curdata_gt = flipud(data_gt);
             [data_seg_ap, data_gt_seg_ap] = XxDataSeg_ForTrain(curdata, curdata_gt, ...
-                round(TotalSegNum/train_cell_num/snr_num/4) , SegX, SegY, RotFlag);
+                ceil(TotalSegNum/train_cell_num/snr_num/4) , SegX, SegY, RotFlag);
             num_image = size(data_gt_seg_ap, 3);
             data_seg(:, :, end+1:end+num_image*n_perSIM) = data_seg_ap;
             data_gt_seg(:, :, end+1:end+num_image) = data_gt_seg_ap;
@@ -209,7 +251,7 @@ for i = train_cell_list
             curdata = fliplr(data);
             curdata_gt = fliplr(data_gt);
             [data_seg_ap, data_gt_seg_ap] = XxDataSeg_ForTrain(curdata, curdata_gt, ...
-                round(TotalSegNum/train_cell_num/snr_num/4) , SegX, SegY, RotFlag);
+                ceil(TotalSegNum/train_cell_num/snr_num/4) , SegX, SegY, RotFlag);
             num_image = size(data_gt_seg_ap, 3);
             data_seg(:, :, end+1:end+num_image*n_perSIM) = data_seg_ap;
             data_gt_seg(:, :, end+1:end+num_image) = data_gt_seg_ap;
@@ -217,7 +259,7 @@ for i = train_cell_list
             curdata = flipud(curdata);
             curdata_gt = flipud(curdata_gt);
             [data_seg_ap, data_gt_seg_ap] = XxDataSeg_ForTrain(curdata, curdata_gt, ...
-                round(TotalSegNum/train_cell_num/snr_num/4) , SegX, SegY, RotFlag);
+                ceil(TotalSegNum/train_cell_num/snr_num/4) , SegX, SegY, RotFlag);
             num_image = size(data_gt_seg_ap, 3);
             data_seg(:, :, end+1:end+num_image*n_perSIM) = data_seg_ap;
             data_gt_seg(:, :, end+1:end+num_image) = data_gt_seg_ap;
